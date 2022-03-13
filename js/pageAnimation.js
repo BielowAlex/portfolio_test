dcc220bc11c37bb7af7e4572ba464cc1e7914755
BielowAlex <bielow.alex@gmail.com>
2022-03-13 23:03:34 +0200

    new font; Added scroll animation; Added animation for elements from projects list and button see all and gitHub; Added title animation

--- js/pageAnimation.js
+++ js/pageAnimation.js
@@ -1,28 +1,78 @@
-let img = document.getElementById('me');
-let bio = document.getElementById('bio');
-let carH2 = document.getElementById('car_h2');
-let carousel = document.getElementById('carrousel');
-let h2Line = document.getElementById('bio_title_line')
-let bioH2 = document.getElementById('bio_h2')
-let wrapBio = document.querySelector('.wrap');
-let project = document.querySelectorAll('.project')
-let projH2 = document.querySelectorAll('.proj_h2')
-console.log(project);
 
-for (let i = 0; i<project.length;i++) {
-    project[i].onmouseover = function (){
-        // project.classList.add('project_anim');
-        projH2[i].classList.add('proj_h2_anim');
-        project[i].onmouseleave = function (){
-            // project.classList.remove('project_anim');
-            projH2[i].classList.remove('proj_h2_anim');
+const bio = document.getElementById('bio');
+const carH2 = document.getElementById('car_h2');
+
+
+
+
+//============================================================================\\
+//============================Title_Anim======================================\\
+//============================================================================\\
+
+
+const titleList = document.querySelectorAll('.title');
+const lines = document.querySelectorAll('.line');
+// console.log(lines);
+
+for (let i = 0;i<titleList.length;i++) {
+    titleList[i].onmouseover =function (){
+        lines[i].classList.add('line_anim');
+       titleList[i].onmouseleave = () => {
+            lines[i].classList.remove('line_anim');
         }
-
     }
+
 }
 
 
-let start = pageYOffset;
+
+// for (let i = 0; i<project.length;i++) {
+//     project[i].onmouseover = function (){
+//         // project.classList.add('project_anim');
+//         projH2[i].classList.add('proj_h2_anim');
+//         project[i].onmouseleave = function (){
+//             // project.classList.remove('project_anim');
+//             projH2[i].classList.remove('proj_h2_anim');
+//         }
+//
+//     }
+// }
+//============================================================================\\
+//============================Title_Anim======================================\\
+//============================================================================\\
+
+//============================================================================\\
+//============================Project=========================================\\
+//============================================================================\\
+const projImgList = document.querySelectorAll('.proj_img');
+const projLineList = document.querySelectorAll('.proj_line');
+const projectsList = document.querySelectorAll('.project');
+
+for (let i = 0;i<projectsList.length;i++) {
+    projectsList[i].onmouseover = ()=>{
+
+        console.log('lol')
+        projImgList[i].classList.add('proj_img_anim');
+        projLineList[i].classList.add('proj_line_anim');
+    }
+    projectsList[i].onmouseleave = ()=>{
+        projImgList[i].classList.remove('proj_img_anim');
+        projLineList[i].classList.remove('proj_line_anim');
+    }
+}
+
+//============================================================================\\
+//============================Project=========================================\\
+//============================================================================\\
+
+//============================================================================\\
+//============================Biography=======================================\\
+//============================================================================\\
+const img = document.getElementById('me');
+img.classList.add('img__anim');
+
+const start = pageYOffset;
+const wrapBio = document.querySelector('.wrap');
 
 window.addEventListener('scroll',function (){
     if(pageYOffset>start){
@@ -32,10 +82,23 @@ window.addEventListener('scroll',function (){
     }
 })
 
+const bioH2 = document.getElementById('bio_h2')
+const bioH2Line = document.getElementById('bio_title_line')
 
+bioH2.onmouseover = function () {
+    bioH2Line.classList.add('line_anim');
+    bioH2.onmouseleave = () => bioH2Line.classList.remove('line_anim');
+}
+//============================================================================\\
+//============================Bio_Wrap_Visible================================\\
+//============================================================================\\
+
+
+let carousel = document.getElementById('carrousel');
 carousel.classList.add('carrousel_anim');
 
-img.classList.add('img__anim');
+
+
 
 function bioShow() {
     bio.classList.add('bio__anim');
@@ -47,13 +110,9 @@ function carH2Show() {
     carH2.classList.add('car_h2_anim');
 }
 
-setTimeout(carH2Show, 2000);
+carH2Show();
 
-bioH2.onmouseover = function () {
-    console.log("pizda")
-    h2Line.classList.add('line_anim');
-    bioH2.onmouseleave = () => h2Line.classList.remove('line_anim');
-}
+
 
 
 
